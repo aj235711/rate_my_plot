927c23a54810e46c6625baf5ea9715777fe118af
aj235711 <amirjaved68504@gmail.com>
2021-11-13 22:03:42 +0530

    testing server

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 import Head from "next/head";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
   return <></>;
 }
+
+export const getStaticProps = async () => {
+  const res = await axios.get(
+    (process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : "https://rate-my-plot-20krv1kdy-aj235711.vercel.app/") + "/api/hello"
+  );
+  console.log(res);
+  return {
+    props: {
+      data: res.data,
+    },
+  };
+};
